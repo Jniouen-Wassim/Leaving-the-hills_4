fce91ab5c9afe43fc85d166bdbf768d8f1d240e4
Jniouen-Wassim <jniouenwassim@gmail.com>
2020-12-21 21:22:18 +0100

    commit

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -1,10 +1,111 @@
-let start = document.getElementById('start')
-let cycle = document.getElementById('cycle')
-let pause = document.getElementById('pause')
+////////////////////////////////( Initialisation )////////////////////////////////////////////////////////////////
+
+//--------( 'let' for => 'click custom' & 'Minuteur' )---------//
 const body = document.querySelector('body')
 
+let start = document.getElementById('start');
+let pause = document.getElementById('pause');
+let reset = document.getElementById('reset');
+
+//--------( 'let' for => Minuteur' )---------//
+let wm = document.getElementById('workMinutes');
+let ws = document.getElementById('workSeconds');
+
+let bm = document.getElementById('breakMinutes');
+let bs = document.getElementById('breakSeconds');
+
+let startTimer;
+
+///////////////////////////////( click custom )///////////////////////////////////////////////////
 
 start.addEventListener('click', function () {
-    body.style.backgroundColor = 'green'
+    
+    body.style.filter = "none";
+    start.style.filter= "none";
+    reset.style.filter = "brightness(180%)";
+    pause.style.filter = "brightness(180%)";
 
 })
+
+pause.addEventListener('click', function () {
+    
+    body.style.filter = "none";
+    pause.style.filter = "none";
+    reset.style.filter = "brightness(180%)";
+    start.style.filter = "brightness(180%)";
+
+})
+
+reset.addEventListener('click', function () {
+    
+    body.style.filter = "grayscale(60%)";
+    reset.style.filter = "none";
+    start.style.filter = "brightness(180%)";
+    pause.style.filter = "brightness(180%)";
+
+})
+
+////////////////////////////( Minuteur )/////////////////////////////////////////////
+
+
+
+start.addEventListener('click', function(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer, 1000)
+    } else {
+        alert("Timer is already running");
+    }
+})
+
+reset.addEventListener('click', function(){
+    wm.innerText = 25;
+    ws.innerText = "00";
+
+    bm.innerText = 5;
+    bs.innerText = "00";
+
+    document.getElementById('counter').innerText = 0;
+    pauseInterval()
+    startTimer = undefined;
+})
+
+pause.addEventListener('click', function(){
+    pauseInterval()
+    startTimer = undefined;
+})
+
+
+
+function timer(){
+    //work timer counter
+    if(ws.innerText != 0){
+        ws.innerText--;
+    } else if(wm.innerText != 0 && ws.innerText == 0){
+        ws.innerText = 59;
+        wm.innerText--;
+    }
+
+    //pause timer counter
+    if(wm.innerText == 0 && ws.innerText == 0){
+        if(bs.innerText != 0){
+            bs.innerText--;
+        } else if(bm.innerText != 0 && bs.innerText == 0){
+            bs.innerText = 59;
+            bm.innerText--;
+        }
+    }
+
+
+    if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
+        wm.innerText = 25;
+        ws.innerText = "00";
+
+        bm.innerText = 5;
+        bs.innerText = "00";
+
+        document.getElementById('counter').innerText++;
+    }
+}
+function pauseInterval(){
+    clearInterval(startTimer);
+}
--- script.js
+++ script.js
@@ -1,2 +1,74 @@
 
-console.log("salut");
+//================================= Début Collection ============================================//
+let collection = [{
+ logo: "logo-Neon fab fa-10x fa-markdown" ,
+ link:"./MarkdownEditor/markdown.html", 
+ title: "MARKDOWN EDITOR",
+},
+
+{
+  logo: "logo-Neon far fa-10x fa-clock" ,
+  link:"./Pomodoro/pomodoro.html", 
+  title: "POMODOROTIMER",
+ },
+
+ {
+  logo: "logo-Neon far fa-10x fa-window-maximize" ,
+  link:"./Horloge-Time/Horloge.html", 
+  title: "LANDING PAGE",
+ },
+
+ {
+  logo: "logo-Neon fas fa-10x fa-question-circle" ,
+  link:"../Quizz-Api/quizz-Api.html", 
+  title: "QUIZZ",
+ },
+
+ {
+  logo: "logo-Neon fas fa-10x fa-paint-brush",
+  link:"./Paint/paint.html", 
+  title: "Paint",
+ },
+];
+//================================= Collection Fin ============================================//
+//================================= Début DOM  ============================================//
+const sectionContainer = document.querySelector("section");
+sectionContainer.innerHTML= "" ;
+
+for (let element of collection ) {
+  
+  let cardBody = document.createElement('div')
+  cardBody.className = "cardBody col-sm-6 col-lg-2 col-xl-2 mt-4 text-center"
+  sectionContainer.appendChild(cardBody)
+
+//------------------( Div: Logo )------------------//
+
+  let Logo = document.createElement('div')
+  Logo.className = "logo"
+  cardBody.appendChild(Logo)
+
+  let a_Logo = document.createElement('a')
+  a_Logo.className = "aLogo"
+  a_Logo.href = element.link
+  //a_Logo.target = "_blank"
+  Logo.appendChild(a_Logo)
+
+  let i_Logo = document.createElement('i')
+  i_Logo.className = element.logo
+  a_Logo.appendChild(i_Logo)
+
+  //------------------( Div: Text )------------------//
+  
+  let Text = document.createElement('div')
+  cardBody.appendChild(Text)
+
+  let a_Text = document.createElement('a')
+  a_Text.className = "aText btn mt-4"
+  a_Text.href = element.link
+  //a_Text.target = "_blank"
+  a_Text.textContent = element.title;
+  Text.appendChild(a_Text)
+ 
+}
+//================================= DOM fin ============================================//
+//================================= "click Event" ============================================//
