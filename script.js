acfac75416796909f87e1a0c9974d9e5721d1d77
Jniouen-Wassim <jniouenwassim@gmail.com>
2021-02-04 22:04:13 +0100

    commit

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
-
 //================================= Début Collection ============================================//
 
 let collection = [{
- logo: "logo-Neon fab fa-10x fa-markdown" ,
- link:"./MarkdownEditor/markdown.html", 
- title: "MARKDOWN EDITOR",
-},
+    logo: "logo-Neon far fa-10x fa-window-maximize",
+    link: "./Horloge-Time/Horloge.html",
+    title: "LANDING PAGE",
+  },
 
-{
-  logo: "logo-Neon far fa-10x fa-clock" ,
-  link:"./Pomodoro/pomodoro.html", 
-  title: "POMODOROTIMER",
- },
+  {
+    logo: "logo-Neon far fa-10x fa-clock",
+    link: "./Pomodoro/pomodoro.html",
+    title: "POMODOROTIMER",
+  },
 
-
-
- {
-  logo: "logo-Neon far fa-10x fa-window-maximize" ,
-  link:"./Horloge-Time/Horloge.html", 
-  title: "LANDING PAGE",
- },
+  {
+    logo: "logo-Neon2 fas fa-10x fa-exchange-alt",
+    link: "https://jniouen-wassim.github.io/My-portfolio/",
+    title: "PORTFOLIO",
+  },
 
 
- {
-  logo: "logo-Neon fas fa-10x fa-paint-brush",
-  link:"./Paint/paint.html", 
-  title: "PAINT",
- },
+  {
+    logo: "logo-Neon fab fa-10x fa-markdown",
+    link: "./MarkdownEditor/markdown.html",
+    title: "MARKDOWN EDITOR",
+  },
 
- {
-  logo: "logo-Neon fas fa-10x fa-undo-alt",
-  link:"https://jniouen-wassim.github.io/My-portfolio/", 
-  title: "PORTFOLIO",
- },
+  {
+    logo: "logo-Neon fas fa-10x fa-paint-brush",
+    link: "./Paint/paint.html",
+    title: "PAINT",
+  },
+
+
 ];
 
 //=======================================================================( "Début DOM" )==========================================//
 
 const sectionContainer = document.querySelector("section");
-sectionContainer.innerHTML= "" ;
+sectionContainer.innerHTML = "";
 
-let i = 0 ;
+let i = 1;
 
-for (let element of collection ) {
-  
+for (let element of collection) {
+
   let cardBody = document.createElement('div')
   cardBody.className = "cardBody col-sm-6 col-lg-2 col-xl-2 mt-4 text-center"
   sectionContainer.appendChild(cardBody)
 
-//------------------( Div: Logo )------------------//
+  //------------------( Div: Logo )------------------//
 
   let Logo = document.createElement('div')
   Logo.className = "logo"
@@ -66,19 +65,20 @@ for (let element of collection ) {
   a_Logo.appendChild(i_Logo)
 
   //------------------( Div: Text )------------------//
-  
+
   let Text = document.createElement('div')
   Text.id = `containerIcon${i}`
   console.log(Text.id);
   cardBody.appendChild(Text)
 
   let a_Text = document.createElement('a')
+  a_Text.id = `aText${i}`
   a_Text.className = "aText btn mt-4"
   a_Text.href = element.link
   a_Text.target = "_blank"
   a_Text.textContent = element.title;
   Text.appendChild(a_Text)
- 
+
   i++;
 }
 
@@ -86,7 +86,7 @@ for (let element of collection ) {
 
 
 // for (let y=0 ; y < collection.length ; y++) {
- 
+
 //   let a_containerIcon = document.getElementById(`containerIcon${y}`);
 //   let i_voirIcon = document.getElementById(`voirIcon${i}`);
 
@@ -94,15 +94,14 @@ for (let element of collection ) {
 //   a_containerIcon.addEventListener('mouseover', function () {
 //     a_containerIcon.style.backgroundColor = "red";
 //     /* i_voirIcon.style.filter = "filter:blur(0px);"  Pour que le logo réapparaît */
-    
+
 // })
 
 //   a_containerIcon.addEventListener('mouseout', function () {
-    
+
 //     a_containerIcon.style.backgroundColor = "transparent";
 //     /* i_voirIcon.style.filter = "filter:blur(200px);"  Pour que le logo disparaît */
-  
+
 // })
 
 // }
-
